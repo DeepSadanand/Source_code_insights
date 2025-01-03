2f5918f6ed44fb46ef34e175314f4a2eff3e4a8a
Sadanand Howal <howal.sada@gmail.com>
2025-01-03 11:08:56 +0530

    modified

--- src/helper.py
+++ src/helper.py
@@ -1,24 +1,28 @@
 import os
 from git import Repo
 
-from langchain.document_loaders.generic import GenericLoader
-from langchain.document_loaders.parsers import LanguageParser # to parse language
+from langchain_community.document_loaders.generic import GenericLoader
+#from langchain.document_loaders.parsers import LanguageParser # to parse language
+
+from langchain_community.document_loaders.parsers.language.language_parser import LanguageParser
 
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.text_splitter import Language
 
-from langchain.embeddings import OpenAIEmbeddings
+#from langchain.embeddings import OpenAIEmbeddings # Depreciated
+
+from langchain_openai import OpenAIEmbeddings
 
 def repo_ingestion(repo_url):
     os.makedirs("/repo",exist_ok = True)
     repo_path = "repo/"
     Repo.clone_from(repo_url,repo_path)
 
-def repo_load():
-    loader = GenericLoader(repo_path, 
+def repo_load(repo_path):
+    loader = GenericLoader.from_filesystem(repo_path, 
                              glob="**/**",
                              suffixes = [".py"],
-                             parser = LanguageParser(Language = "python",parser_threshold = 200),
+                             parser = LanguageParser(language = "python",parser_threshold = 200),
                              )
     documents = loader.load()
 
@@ -38,5 +42,5 @@ def text_splitter(documents):
 
 def load_embedding():
 
-    embeddings = OpenAIEmbeddings(disallowed_special = 1)
+    embeddings = OpenAIEmbeddings(disallowed_special = "all")
     return embeddings
---
+++ templates/store_vector.py
@@ -0,0 +1,19 @@
+from src.helper import repo_ingestion, repo_load, load_embedding, text_splitter
+from dotenv import load_dotenv
+
+from langchain_community.vectorstores import Chroma
+
+import os
+
+env_var  = load_dotenv()
+
+OPENAI_API_KEY = os.environ.get('OPENAI_API_KEY')
+os.environ['OPENAI_API_KEY'] = OPENAI_API_KEY
+
+documents = repo_load("repo/")
+text_chunks= text_splitter(documents)
+embeddings = load_embedding()
+
+
+vectorDB = Chroma.from_documents(text_chunks, embedding=embeddings,persist_directory= "/db")
+vectorDB.persist()
